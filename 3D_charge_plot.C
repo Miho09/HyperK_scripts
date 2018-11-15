982eba6aad285b93e8cb659a1b8ede366a6505c0
Miho Wakai <miho.wakai@desy.de>
2018-11-15 15:54:46 +0000

    added charged plot

---
+++ 3D_charge_plot.C
@@ -0,0 +1,148 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void PMT_timing_position_per_event(char *filename=NULL) {
+  /* A simple script to plot aspects of phototube hits
+   * This code is rather cavalier; I should be checking return values, etc.
+   * First revision 6-24-10 David Webber
+   *
+   * I like to run this macro as
+   * $ root -l -x 'read_PMT.C("../wcsim.root")'
+   */
+
+
+
+  gROOT->Reset();
+  char* wcsimdirenv;
+  wcsimdirenv = getenv ("WCSIMDIR");
+  if(wcsimdirenv !=  NULL){
+    gSystem->Load("${WCSIMDIR}/libWCSimRoot.so");
+  }else{
+    gSystem->Load("../libWCSimRoot.so");
+  }
+  gStyle->SetOptStat(1);
+
+  TFile *f;
+  if (filename==NULL){
+    f = new TFile("../wcsim.root");
+  }else{
+    f = new TFile(filename);
+  }
+  if (!f->IsOpen()){
+    cout << "Error, could not open input file: " << filename << endl;
+    return -1;
+  }
+
+  TTree  *wcsimT = f->Get("wcsimT");
+
+  WCSimRootEvent *wcsimroothyperevent = new WCSimRootEvent();
+  wcsimT->SetBranchAddress("wcsimrootevent",&wcsimroothyperevent);
+
+  TTree  *wcsimGeoT = (TTree*) f->Get("wcsimGeoT");
+
+  WCSimRootGeom *wcsimrootgeom = 0;
+  wcsimGeoT->SetBranchAddress("wcsimrootgeom",&wcsimrootgeom);
+  // cout << "wcsimrootgeom value: " << wcsimrootgeom << endl;
+  // cout << "getentry: " << wcsimGeoT->GetEntries() << endl;
+  wcsimGeoT->GetEntry(0);
+
+  // Force deletion to prevent memory leak when issuing multiple
+  // calls to GetEvent()
+  wcsimT->GetBranch("wcsimrootevent")->SetAutoDelete(kTRUE);
+
+
+  //--------------------------
+  // As you can see, there are lots of ways to get the number of hits.
+  cout << "Nb of entries " << wcsimT->GetEntries() << endl;
+
+  //-----------------------
+
+  TH3D *coor_q = =new TH3D("coor_q", "Cartesian coordinates with charge",1000,-4000,4000,1000,-4000,4000,1000,-4000,4000);
+  coor_q->SetYTitle("Charge distribution in 3D");
+  // graph.GetYaxis()->SetTitleOffset(1.4);
+  // coor_q->SetXTitle("charge");
+
+
+TFile mihofile("test.root", "RECREATE");
+TTree mihotree("data","data");
+
+float x,y,z,q,t;
+long unsigned int evt = 0;
+mihotree.Branch("x",&x,"x/F");
+mihotree.Branch("y",&y,"y/F");
+mihotree.Branch("z",&z,"z/F");
+mihotree.Branch("q",&q,"q/F");
+mihotree.Branch("t",&t,"t/F");
+mihotree.Branch("evt",&evt,"evt/I");
+
+  const long unsigned int nbEntries = wcsimT->GetEntries();
+
+  for(long unsigned int iEntry = 0; iEntry < nbEntries; iEntry++){
+    // Point to event iEntry inside WCSimTree
+    wcsimT->GetEvent(iEntry);
+    evt = iEntry;
+
+    // Nb of Trigger inside the event
+    const unsigned int nbTriggers = wcsimroothyperevent->GetNumberOfEvents();
+    const unsigned int nbSubTriggers = wcsimroothyperevent->GetNumberOfSubEvents();
+
+    for(long unsigned int iTrig = 0; iTrig < nbTriggers; iTrig++){
+      WCSimRootTrigger *wcsimrootevent = wcsimroothyperevent->GetTrigger(iTrig);
+
+      // RAW HITS
+      int ncherenkovdigihits = wcsimrootevent->GetNcherenkovdigihits();
+        for (int i = 0; i < ncherenkovdigihits; i++){
+          WCSimRootCherenkovDigiHit *hit = (WCSimRootCherenkovDigiHit*)
+          (wcsimrootevent->GetCherenkovDigiHits()->At(i));
+      //WCSimRootChernkovDigiHit has methods GetTubeId(), GetT(), GetQ()
+            // WCSimRootCherenkovHitTime *cHitTime = wcsimrootevent->GetCherenkovHitTimes()->At(i);
+            //WCSimRootCherenkovHitTime has methods GetTubeId(), GetTruetime()
+
+            WCSimRootCherenkovDigiHit *cDigiHit = wcsimrootevent->GetCherenkovDigiHits()->At(i);
+            //WCSimRootChernkovDigiHit has methods GetTubeId(), GetT(), GetQ()
+            QvsT->Fill(cDigiHit->GetT(), cDigiHit->GetQ());
+
+          double charge = hit->GetQ();
+          int tubeId = hit -> GetTubeId();
+          double timing = hit->GetT();
+          // cout << "Tube ID: " << tubeId << endl;
+          WCSimRootPMT pmt = wcsimrootgeom->GetPMT(tubeId);
+          double pmtX = pmt.GetPosition(0);
+          double pmtY = pmt.GetPosition(1);
+          double pmtZ = pmt.GetPosition(2);
+
+          x=pmtX;
+          y=pmtY;
+          z=pmtZ;
+          q=charge;
+          t=timing;
+
+          mihotree.Fill();
+
+          // cout << "Y value: " << pmtY << endl;
+
+          coor_q->Fill(pmtX, pmtY, pmtZ, charge);
+
+          } // END FOR RAW HITS
+
+    } // END FOR iTRIG
+
+  } // END FOR iENTRY
+
+
+  TH1 *temp;
+    TCanvas *c1 = new TCanvas("c1","c1",800,800);
+
+    // c1->cd(1);
+    // QvsT->Draw("colz");
+
+    // c1->cd(1);
+    coor_q->Draw("colz");
+
+
+    mihotree.Write();
+
+
+  }
