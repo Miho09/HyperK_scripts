5157312c9befa2ba9660f6fedf09203b206de3a2
Miho Wakai <miho.wakai@desy.de>
2018-11-28 18:18:21 +0000

    added distribution

--- distribution.C
+++ distribution.C
@@ -64,7 +64,7 @@ void distribution(char *filename=NULL) {
 
 
 
-  TH3D *discorr = new TH3D("coorq", "Cartesian coordinates with charge",1000,-4000,4000,1000,-4000,4000,1000,-4000,4000);
+  TH3D *discorr = new TH3D("coorq", "Cartesian coordinates with charge",100,-4000,4000,100,-4000,4000,100,-4000,4000);
   // cout << "1" << endl;
   discorr->SetTitle("Charge Distribution");
   // cout << "2" << endl;
