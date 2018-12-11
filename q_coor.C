aa65aabae107a695baf1e0b52bfc3562f768d01c
Miho Wakai <miho.wakai@desy.de>
2018-12-11 16:41:10 +0000

    addd

--- q_coor.C
+++ q_coor.C
@@ -65,7 +65,7 @@ void q_coor(char *filename=NULL) {
   // As you can see, there are lots of ways to get the number of hits.
   cout << "Nb of entries " << wcsimT->GetEntries() << endl;
 
-  TH1 *q_raw_hist = new TH1D("q_raw_hist", "hist",100, -1000, 1000);
+  TH1 *q_raw_hist = new TH1D("q_raw_hist", "hist",100, -50, 50);
   q_raw_hist->SetTitle("Histogram for q_raw");
   // coor_q->SetMinimum(1e-11);
   //
